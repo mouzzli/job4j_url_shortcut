5c578cc6cf73e6c04fbf77d11b8b21320e306d95
Mouzzli <cokes03@yandex.ru>
2023-04-28 18:56:48 +0300

    init

---
+++ src/main/java/ru/jobj4/url_shortcut/UrlShortcutApplication.java
@@ -0,0 +1,13 @@
+package ru.jobj4.url_shortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortcutApplication.class, args);
+	}
+
+}
